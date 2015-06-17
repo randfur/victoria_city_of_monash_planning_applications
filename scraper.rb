84b70c5d5469cf857799cfb3f645643994306d91
Alan Cutter <alancutter@chromium.org>
2015-06-17 14:48:15 +1000

    Click the link

--- scraper.rb
+++ scraper.rb
@@ -12,13 +12,8 @@ require 'mechanize'
 
 agent = Mechanize.new
 
-# Read in a page
-page = agent.get(
-    uri = 'https://epathway.monash.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquirySummaryView.aspx',
-    headers = {
-      'Referer' => 'https://epathway.monash.vic.gov.au/ePathway/Production/Web/default.aspx',
-      'Cookie' => 'ePathway=54F04B11E551E20DC2FDCA5A8DDE7AFBE40E7641D6617DE64529D75EE2F0E8012FAAE994740D24FD440648401764B00ADB687356382326F6A4196908F85903516BAC9F61A5C6F3AB032C23701FFCD900D07F7DE281E0709B72C118F1AD826A2AA3FC46FCB05201CC037CC8E1237444DCCF1358B376696C5F89B39840D22BF71D6DD882BDDD3E8018985F81F2BC199DA66D9DE4B01AE3F81B16AF92FB5F1227DD30F28CAB41712B66269631905F835761C01A1729305EA59E69996D700E7BD5E9EE492C485E250BED698D70E47535D142188F370D5B5C97955C1E59BEF81666D67EFCF227183B450E97541CC137BCF6C37037A786D43DC01E97EE8E018C419B20; ASP.NET_SessionId=icrmlzygogfseucnzqkhxcgl',
-    })
+page = agent.get('https://epathway.monash.vic.gov.au/ePathway/Production/Web/Default.aspx')
+page.click('planning applications that we are currently advertising') # Can't access page directly, need to follow link.
 
 puts 'Error:'
 p page.at('#ctl00_MainBodyContent_mErrorPanel')
